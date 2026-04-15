6edd8e55bdd12a0be9189a143848a93163d9f471
Rushikesh <rushijadhav1423@gmail.com>
2026-04-15 13:49:39 +0530

    db-postgres connected

--- index.js
+++ index.js
@@ -22,13 +22,10 @@ const port = process.env.PORT || 8080;
 // the pooler will keep that connection open for sometime to other clients to reuse
 const pool = new pg.Pool({
   host: "localhost",
-  port: 5433,
+  port: 5432,
   user: "postgres",
   password: "postgres",
-  database: "sql_class_2_db",
-  max: 20,
-  connectionTimeoutMillis: 0,
-  idleTimeoutMillis: 0,
+  database: "bookmyticket",
 });
 
 const app = new express();
